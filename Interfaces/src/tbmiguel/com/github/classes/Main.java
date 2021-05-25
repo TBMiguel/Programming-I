ed6ee2f99d153cc2d01c077d46cd93ea73f8eabc
Miguel Junior <69019381+TBMiguel@users.noreply.github.com>
2021-05-24 23:36:17 -0300

    Add files via upload
    Implementation of interfaces in Java Language.

---
+++ Interfaces/src/tbmiguel/com/github/classes/Main.java
@@ -0,0 +1,16 @@
+package tbmiguel.com.github.classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Quadrado quadrado = new Quadrado();
+
+		quadrado.setLado(5);
+		quadrado.getLado();
+		quadrado.calculaArea();
+		quadrado.calculaPerimetro();
+
+	}
+
+}
---
+++ Interfaces/src/tbmiguel/com/github/classes/Triangulo.java
@@ -0,0 +1,24 @@
+package tbmiguel.com.github.classes;
+
+import tbmiguel.com.github.interfaces.FiguraGeometrica;
+
+public class Triangulo implements FiguraGeometrica {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	@Override
+	public void calculaArea() {
+		
+		
+	}
+
+	@Override
+	public void calculaPerimetro() {
+		
+		
+	}
+
+}
